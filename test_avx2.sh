a7bdc13df118248c388c12e5fad456311a39458b
c0deb0t <daniel.liu02@gmail.com>
2021-07-06 21:45:41 -0700

    Fixed doctest target cpu native

--- test_avx2.sh
+++ test_avx2.sh
@@ -1,2 +1,2 @@
 RUSTFLAGS="-C target-cpu=native" cargo test --all-targets -- "$@"
-RUSTFLAGS="-C target-cpu=native" cargo test --doc -- "$@"
+RUSTFLAGS="-C target-cpu=native" RUSTDOCFLAGS="-C target-cpu=native" cargo test --doc -- "$@"
