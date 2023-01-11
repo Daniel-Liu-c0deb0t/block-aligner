2aafb06df58c791ec0a6660d39dcc39d5492157d
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-11 11:52:52 +0100

    Put cbindgen dependency behind a default-enabled feature flag

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
-use cbindgen;
-
-use std::env;
-
 fn main() {
-    if env::var("BLOCK_ALIGNER_C").is_ok() {
-        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-        cbindgen::generate(&crate_dir)
-            .unwrap()
-            .write_to_file(format!("{}/c/block_aligner.h", crate_dir));
+    #[cfg(feature = "cbindgen")]
+    {
+        if std::env::var("BLOCK_ALIGNER_C").is_ok() {
+            let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+            cbindgen::generate(&crate_dir)
+                .unwrap()
+                .write_to_file(format!("{}/c/block_aligner.h", crate_dir));
+        }
     }
 }
