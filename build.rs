b41476fcaeb84bd9f44bcbbfd6593a2c62b1304e
c0deb0t <daniel.liu02@gmail.com>
2023-01-15 17:37:29 -0800

    Use feature flag for c bindings

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
-use cbindgen;
-
-use std::env;
-
 fn main() {
-    if env::var("BLOCK_ALIGNER_C").is_ok() {
-        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    #[cfg(feature = "c_bindings")]
+    {
+        let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let file = format!("{}/c/block_aligner.h", crate_dir);
         cbindgen::generate(&crate_dir)
             .unwrap()
-            .write_to_file(format!("{}/c/block_aligner.h", crate_dir));
+            .write_to_file(&file);
+        println!("cargo:rerun-if-changed={}", file);
     }
 }
